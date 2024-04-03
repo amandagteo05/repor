6998ee24d409801c89555398ca09605475f70a6b
amandagteo05 <148063834+amandagteo05@users.noreply.github.com>
2024-04-03 09:18:49 -0300

    .

--- main.js
+++ main.js
@@ -15,13 +15,13 @@ for(let i=0;i <botoes.length;i++){
     }
 }
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-03-25T00:00:00");
+const tempoObjetivo1 = new Date("2024-12-18T00:00:00");
 
-const tempoObjetivo2 = new Date("2024-03-25T00:00:00");
+const tempoObjetivo2 = new Date("2025-03-01T00:00:00");
 
-const tempoObjetivo3 = new Date("2024-03-25T00:00:00");
+const tempoObjetivo3 = new Date("2027-03-25T00:00:00");
 
-const tempoObjetivo4 = new Date("2024-03-25T00:00:00");
+const tempoObjetivo4 = new Date("2025-01-10T00:00:00");
 
 
 function calculaTempo(tempoObjetivo) {
